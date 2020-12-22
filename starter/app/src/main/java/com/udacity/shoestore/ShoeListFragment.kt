7536e2443cfc1ca3c4a0423b130e41fee2d2308b
Ivan Mang <ivanmang1997@gmail.com>
2020-12-22 14:43:52 +0800

    Added iterable view

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -6,28 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.Shoe
+import kotlinx.android.synthetic.main.sample_shoe_view.view.*
 
 class ShoeListFragment : Fragment() {
 
     private lateinit var viewModel: ShoeListViewModel
+    private lateinit var binding: FragmentShoeListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.i("ShoeListFragment", "Called ViewModel ")
         val binding : FragmentShoeListBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_shoe_list, container, false)
         viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
 
+        binding.shoeListViewModel = viewModel
+
         viewModel.listOfShoe.observe(viewLifecycleOwner, Observer { newList ->
-            binding.shoeName.text = newList.get(1).name
+            newList.forEach { shoe ->
+                val view: View = inflater.inflate(R.layout.sample_shoe_view, null, false)
+                view.shoe_name.text = shoe.name
+                view.shoe_company.text = shoe.company
+                binding.shoeListLayout.addView(view)
+            }
+
         })
         return binding.root
     }
+
 
 }
